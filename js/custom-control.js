c51b00c8816e45d1177491feeaeffb0e7b7d4e3b
Tekson <teksondada@gmail.com>
2019-08-03 16:43:57 +0545

    osm tilelayer added

---
+++ js/custom-control.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([28.2521, 83.9774], 18);
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
