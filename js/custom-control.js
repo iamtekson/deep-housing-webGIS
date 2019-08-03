fc0a0102c23162ad42fd8a87229d2eba877cafd9
Tekson <teksondada@gmail.com>
2019-08-03 16:46:39 +0545

    scale control is added to map

--- js/custom-control.js
+++ js/custom-control.js
@@ -3,3 +3,5 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var scale = L.control.scale().addTo(map);
