a5925e67b0426c5c701651efc98c7c0d8438ca1a
Tekson <teksondada@gmail.com>
2019-08-04 10:51:19 +0545

    Toggle full browser screen added

--- js/control-function.js
+++ js/control-function.js
@@ -1,13 +1,24 @@
 // nav-web functional
-$('.nav-web .options').click(function(){
-   $('.option-content').toggle(); 
+$('.nav-web .options').click(function () {
+   $('.option-content').toggle();
 });
 
 //Zoom in Zoom out function
-$('.zoom-in').click(function(){
-   map.setZoom(map.getZoom()+1)
+$('.zoom-in').click(function () {
+   map.setZoom(map.getZoom() + 1)
 });
-$('.zoom-out').click(function(){
-   map.setZoom(map.getZoom()-1)
+$('.zoom-out').click(function () {
+   map.setZoom(map.getZoom() - 1)
 });
+
+//Toggle full layer
+function fullScreenTgl() {
+   let doc = document, elm = doc.documentElement;
+   if (elm.requestFullscreen) { (!doc.fullscreenElement ? elm.requestFullscreen() : doc.exitFullscreen()) }
+   else if (elm.mozRequestFullScreen) { (!doc.mozFullScreen ? elm.mozRequestFullScreen() : doc.mozCancelFullScreen()) }
+   else if (elm.msRequestFullscreen) { (!doc.msFullscreenElement ? elm.msRequestFullscreen() : doc.msExitFullscreen()) }
+   else if (elm.webkitRequestFullscreen) { (!doc.webkitIsFullscreen ? elm.webkitRequestFullscreen() : doc.webkitCancelFullscreen()) }
+   else { console.log("Fullscreen support not detected."); }
+}
+$('.full-browser').click(fullScreenTgl); //View in full screen toggler
 
