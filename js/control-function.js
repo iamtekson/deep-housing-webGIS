b1c1d64257bdd77eee3de02ac558c7ff0d16f0be
Tekson <teksondada@gmail.com>
2019-08-04 10:48:23 +0545

    The Zoomin Zoomout function added

--- js/control-function.js
+++ js/control-function.js
@@ -1,4 +1,13 @@
-
+// nav-web functional
 $('.nav-web .options').click(function(){
    $('.option-content').toggle(); 
 });
+
+//Zoom in Zoom out function
+$('.zoom-in').click(function(){
+   map.setZoom(map.getZoom()+1)
+});
+$('.zoom-out').click(function(){
+   map.setZoom(map.getZoom()-1)
+});
+
