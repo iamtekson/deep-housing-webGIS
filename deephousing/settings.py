60fd09881dde310ee092e9f6b6810b7383d9498d
Tekson <teksondada@gmail.com>
2019-08-06 16:02:14 +0545

    static file directory added to setting.py

--- deephousing/settings.py
+++ deephousing/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
+    'django.contrib.gis',
+    'leaflet',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_DIR = [
+    os.path.join(BASE_DIR, 'deephousing/static')
+]
