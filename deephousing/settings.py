9605f7db1fce5f99281caf7365cba6af89b9fb03
Tekson <teksondada@gmail.com>
2019-08-06 17:30:31 +0545

    Manage templates and the urls

--- deephousing/settings.py
+++ deephousing/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'deephousing/static')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def webmap(request):
+    return render(request, 'pages/webmap.html')
