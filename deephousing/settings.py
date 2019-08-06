cdaab47b3a198a6aff04a30bfa81f0767597aad0
Tekson <teksondada@gmail.com>
2019-08-06 15:06:36 +0545

    postgis database connected

--- deephousing/settings.py
+++ deephousing/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'deephousing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'OPTIONS': {
+            'options': '-c search-path=data,public'
+        },
+        'NAME': 'deephousing',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD':'admin',
     }
 }
 
