8ed4dae5e03dae6dfef66936fb74bca5149a7e86
Tekson <teksondada@gmail.com>
2019-08-08 12:48:16 +0545

    data url added to urls.py

--- shapefiles/urls.py
+++ shapefiles/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('boundary', views.boundary)
+    path('boundary', views.boundary),
+    path('boundarybuilding', views.boundarybuilding),
+    path('drainage', views.drainage),
+    path('drainageoutletvalve', views.drainageOutletValve),
+    path('park', views.park),
+    path('road', views.road),
+    path('sanitary', views.sanitary),
+    path('sanitaryjunctionpoint', views.sanitaryJunctionPoint),
+    path('sewer', views.sewer),
+    path('sewerjunctionpoints', views.sewerJunctionPoints),
+    path('street', views.street),
+    path('temple', views.temple),
+    path('waternetwork', views.waterNetwork),
 ]
