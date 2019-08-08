112a9b2cd31695f8dbb4358d22992de67bea38c0
Tekson <teksondada@gmail.com>
2019-08-08 12:19:25 +0545

    shapefiles/views.py initiated

--- shapefiles/views.py
+++ shapefiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.core.serilizers import serilizers
+from django.http import HttpResponse
+from .models import Boundary, Boundarybuilding, Drainage, DrainageOutletValve, Park, Road, Sanitary, SanitaryJunctionPoint, Sewer, SewerJunctionPoints, Street, Swimmingpool, Temple, WaterNetwork
 
-# Create your views here.
+def boundary():
+    boundaryData = serilizers('geojson', Boundary.object.all())
+    return HttpResponse(boundaryData, content_type='geojson')
+
