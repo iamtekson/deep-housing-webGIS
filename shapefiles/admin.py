894779bb30e0e8068d95a48de18dbca2411405f2
Tekson <teksondada@gmail.com>
2019-08-08 10:31:53 +0545

    importing models in admin.py

--- shapefiles/admin.py
+++ shapefiles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
-from .models import Boundary
+from .models import Boundary, Boundarybuilding, Drainage, DrainageOutletValve, Park, Road, Sanitary, SanitaryJunctionPoint, Sewer, SewerJunctionPoints, Street, Swimmingpool, Temple, WaterNetwork
 
 class BoundaryAdmin(LeafletGeoAdmin):
     list_display = ('gid', 'area')
