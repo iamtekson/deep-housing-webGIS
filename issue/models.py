db478a7151e879b93fb744bc17de77e852d834b9
Tekson <teksondada@gmail.com>
2019-08-12 10:01:25 +0545

    creating model in issue

--- issue/models.py
+++ issue/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Issue(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    issue_header = models.CharField(max_length=50)
+    issue_body = models.CharField(max_length=500)
+    issue_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
--- issue/views.py
+++ issue/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def issue(request):
+    if request.method == 'POST':
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        issue_header = request.POST['issue_header']
+        issue_body = request.POST['issue_body']
+
+        issue = Issue()
+        
     return render(request, 'pages/issue.html')
