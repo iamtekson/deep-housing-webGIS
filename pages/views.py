876245c2c0b90adb1e491806c3c13c32dd0ac655
Tekson <teksondada@gmail.com>
2019-08-12 10:38:52 +0545

    manage errors in form

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from issue.models import Issue
 
 # Create your views here.
 def index(request):
@@ -8,4 +9,14 @@ def about(request):
     return render(request, 'pages/about.html')
 
 def webmap(request):
+    if request.method == 'POST':
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        issue_header = request.POST['issue_header']
+        issue_body = request.POST['issue_body']
+
+        issue = Issue(first_name=first_name, last_name=last_name, issue_header=issue_header, issue_body=issue_body)
+        issue.save()
+
+        return render(request, 'pages/webmap.html')
     return render(request, 'pages/webmap.html')
