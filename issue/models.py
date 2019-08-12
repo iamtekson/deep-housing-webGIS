4b2d9fa8d24666c93064064ffe1ca6e33bcaf327
Tekson <teksondada@gmail.com>
2019-08-12 10:45:39 +0545

    new migrations applied

--- issue/models.py
+++ issue/models.py
@@ -9,7 +9,7 @@ class Issue(models.Model):
     issue_header = models.CharField(max_length=50)
     issue_body = models.CharField(max_length=500)
     issue_date = models.DateTimeField(auto_now_add=True)
-    location = gis_model.PointField(srid=4326, blank=True)
+    location = gis_model.PointField(srid=4326, blank=True, null=True)
 
     def __str__(self):
         return self.first_name
