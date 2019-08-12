b60a445973476b69661428ab30304b01c5731005
Tekson <teksondada@gmail.com>
2019-08-12 10:03:10 +0545

    location filed added

--- issue/models.py
+++ issue/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_model
 from datetime import datetime
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Issue(models.Model):
     issue_header = models.CharField(max_length=50)
     issue_body = models.CharField(max_length=500)
     issue_date = models.DateTimeField(auto_now_add=True)
+    location = gis_model.PointField(srid=4326, blank=True)
 
     def __str__(self):
         return self.first_name
