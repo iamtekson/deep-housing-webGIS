12e40d96c520dd7bc4e3e8b5d3049187693bf8ec
Tekson <teksondada@gmail.com>
2019-08-14 18:46:49 +0545

    updated file

--- restframework/serializers.py
+++ restframework/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+# from rest_framework_gis.serializers import GeoFeatureModelSerializer, GeometrySerializerModelField
 from shapefiles.models import House
 
-class HouseSerializer(serializers.ModelSerializer):
+class HouseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = House
         fields = ('gid', 'house_type', 'area_sq_m_field', 'curr_resid', 'permanent', 'plot_no', 'geom')
+        geo_field = 'geom'
